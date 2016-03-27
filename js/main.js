e67808da2fe194b403499c5aeed469a690417fd2
ninja <ninja@stuarts-Mac-mini.local>
2016-03-27 16:28:10 -0400

    base files

---
+++ js/main.js
@@ -0,0 +1,184 @@
+ var d = new Date();
+        var weekday = new Array(7);
+        var branchstatus = "closed";
+        var nextchecktime = 0;
+
+        var changedisplay;
+        var showdisplay;
+
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        var today = d.getDay();
+        var branchhours = weekday[today];
+
+        var nowhour = (d.getHours()) * 100;
+        var nowmin = d.getMinutes();
+
+
+        currenttime = nowhour + nowmin;
+
+
+        starttime = kioskObj["days"][branchhours][0];
+        endtime = kioskObj["days"][branchhours][1];
+
+
+        //branch is currently opened?
+        if (starttime != 0) {
+            if (currenttime > starttime && currenttime < endtime) {
+                branchstatus = "opened";
+            }
+
+            //check if before or after opening today
+            if (currenttime < starttime) {
+                nextchecktime = starttime - currenttime; //before open today;
+            } else if (currenttime > endtime) {
+                //branch has closed, see when next time will open.
+                nextopen();
+
+            }
+
+
+        } else {
+            //see when text time will open	
+            nextopen()
+        }
+
+
+
+        checkopened = setInterval(nextdisplay, showdisplay);
+
+
+        if (branchstatus == "closed") {
+            //do play closed
+            playvideo();
+
+        } else {
+            //do play open game  
+            playgame();
+        }
+
+
+
+        function playgame() {
+            console.log("playgame");
+
+        }
+
+        function playvideo() {
+            console.log("playvideo");
+
+        }
+
+        function setclosetime() {
+            console.log("do close");
+            var endtime = kioskObj["days"][branchhours][1];
+            var closetime = endtime - currenttime;
+            var unittime = ((closetime) / 100);
+            showdisplay = getnextdisplay(unittime);
+
+            checkopened = setInterval(nextdisplay, showdisplay);
+
+
+
+
+        }
+
+        function setopentime() {
+            console.log("do opening");
+
+        }
+
+        function nextdisplay() {
+
+            clearInterval(checkopened);
+
+            switch (branchstatus) {
+
+                case "closed":
+                    branchstatus = "opened";
+                    setclosetime();
+                    playgame();
+                    break;
+
+                case "opened":
+                    branchstatus = "closed";
+                    setopentime();
+                    playvideo();
+                    break;
+
+                default:
+                    break;
+
+            }
+
+
+        }
+
+
+
+        function nextopen() {
+
+
+            var endofday = 0;
+            var nextopened = 0;
+
+            howmanydays = 0;
+
+
+
+            for (var i = 1; i < 7; i++) {
+
+                tomorrow = (today + i) % 6;
+                tomorrow_branchhours = weekday[tomorrow];
+                nextopened = kioskObj["days"][tomorrow_branchhours][0];
+                if (nextopened != 0) {
+
+
+                    endofday = 2400 - currenttime
+                    nextopen = kioskObj["days"][tomorrow_branchhours][0];
+
+
+                    break;
+                }
+
+
+
+
+                howmanydays++;
+
+            }
+
+            unittime = ((nextopen + endofday + howmanydays * 2400) / 100);
+            showdisplay = getnextdisplay(unittime);
+            //console.log(showdisplay);
+
+        }
+
+
+
+        function getnextdisplay(unittime) {
+
+            hours = Math.floor(unittime);
+            minutes = hours * 60 + Math.ceil(60 - (100 - ((unittime % hours) * 100)));
+            seconds = minutes * 60;
+            milliseconds = seconds * 1000;
+
+            return Math.floor(milliseconds);
+
+
+        }
+
+        //
+
+        //check for holiday, not implemented yet.
+
+
+        //var checkopened=setInterval(time, 3000);
+
+        console.log(branchstatus);
---
+++ js/object.js
@@ -0,0 +1,18 @@
+
+var kioskObj={
+	"days": {
+		"Monday":[900,1700],
+		"Tuesday":[900,1600],
+		"Wednesday":[900,1600],
+		"Thursday":[900,1600],
+		"Friday":[900,1700],
+		"Saturday":[900,1400],
+		"Sunday":[0,0],
+	},
+	
+	"holidays":[],
+	
+	  "videos":[]
+	 
+		
+};
